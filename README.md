1bc91aa6e19d3b837e0991ee2a9c6675a1a858a9
Rohini Kamade <75832739+Rohini2002-kamade@users.noreply.github.com>
2021-12-27 17:46:46 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+Hello there! 
+I'm Rohini Kamade from CSE 3rd year.
+I enjoy developing websites be it Frontend or Backend.
+I'm conversant with C++ Programming and I have done few hands on and  projects. 
---
+++ project1.js
@@ -0,0 +1,11 @@
+<script>
+let a;
+let date;
+let time;
+setInterval(() => {
+    a = new Date();
+    date = a.toLocaleDateString();
+    time = a.getHours() + ':' + a.getMinutes() + ':' + a.getSeconds();
+    document.getElementById('time').innerHTML = time + "on" + date;
+}, 1000);
+</script>
